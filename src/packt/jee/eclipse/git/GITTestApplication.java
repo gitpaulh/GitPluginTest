523c36e357effbb884987bc76a2e91268595341a
paul git <paulpyhor@gmail.com>
2016-06-17 11:39:49 +0800

    this is initial git commit

---
+++ src/packt/jee/eclipse/git/GITTestApplication.java
@@ -0,0 +1,10 @@
+package packt.jee.eclipse.git;
+
+public class GITTestApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("This is a test for GIT!");
+	}
+
+}
